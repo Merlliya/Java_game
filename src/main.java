787e4b5e29ed24f7755b0f62a9224f89201cf043
Kirlun12 <79675068+Merlliya@users.noreply.github.com>
2021-06-02 21:07:30 +0900

    Финальное исправление
    При открытии проекта в другой программе появилась ошибка, которую пришлось исправить.

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PlayingCard {
+public class main {
     public static void main(String[] args) {
 
         int cardsPerPlayer = 5; // Инициализация переменных
