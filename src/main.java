a5f33b950d99bc185509814e5d33230d31e7a645
Kirlun12 <79675068+Merlliya@users.noreply.github.com>
2021-06-02 19:49:26 +0900

    Инициализация Мастей и Значений
    Через String

--- src/main.java
+++ src/main.java
@@ -5,6 +5,15 @@ public class PlayingCard {
 
         int cardsPerPlayer = 5; // Инициализация переменных
         int players = 0 ;
+        String[] SUITS_LIST = {
+                "Пик", "Бубен", "Черв", "Треф"
+        };
+        String[] RANK_LIST = {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Валет", "Королева", "Король", "Туз"
+        };
+        int n = SUITS_LIST.length * RANK_LIST.length; // Кол-во карт
+
         
     }
 }
