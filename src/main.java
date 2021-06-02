f1ba91feef293605859d1ee406d16dda3914a177
Kirlun12 <79675068+Merlliya@users.noreply.github.com>
2021-06-02 19:41:59 +0900

    Инициализация переменных
    Начальное количество игроков, Количество карт на человека

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
 public class PlayingCard {
-    
+    public static void main(String[] args) {
+
+        int cardsPerPlayer = 5; // Инициализация переменных
+        int players = 0 ;
+        
+    }
 }
